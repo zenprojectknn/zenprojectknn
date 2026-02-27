a074e40ef23ed184f14f9a518d82e2e38e927736
zenprojectknn <zenf44121094@gmail.com>
2026-02-28 01:33:26 +0800

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,1 +1,15 @@
+document.getElementById("loginForm").addEventListener("submit", function(e){
+  e.preventDefault();
 
+  const user = document.getElementById("username").value;
+  const pass = document.getElementById("password").value;
+  const error = document.getElementById("error");
+
+  // contoh akun sederhana
+  if(user === "admin" && pass === "123"){
+    localStorage.setItem("login", "true");
+    window.location.href = "index.html";
+  }else{
+    error.textContent = "Username atau password salah";
+  }
+});
